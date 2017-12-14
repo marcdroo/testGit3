2a29d5e49496bbc9b702fa3056040a42367af9f8
Marc Drouillard <Marc.Drouillard@uhn.ca>
2017-12-14 08:28:44 -0500

    checkin on testbranch

---
+++ fooClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1 {
+    class fooClass {
+
+        public string  GetAString() {
+            return $"from fooClass @ {DateTime.Now}";
+        }
+
+    }
+}
