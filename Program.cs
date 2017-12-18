92488e079e69592daba8a751c5174e5b7c84df6e
Marc Drouillard <Marc.Drouillard@uhn.ca>
2017-12-18 09:13:33 -0500

    added foo

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
             for (int i = 0; i < 10000; i++) { 
-                Console.WriteLine($"{i} - Time is-{new fooClass().GetAString()}");              
+                Console.WriteLine($"{i} - Time is-{new fooClass().GetAString()}");
+                Console.WriteLine("foo");
             }
             Console.ReadLine();
         }
